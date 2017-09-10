90e57f9c11a51b58c6f103f5e6fbb9357555627a
Heng <lhengi888@gmail.com>
2017-09-09 23:13:49 -0500

    Initial Commit

---
+++ cs251/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  cs251
+//
+//  Created by Heng Li on 9/09/17.
+//  Copyright © 2017 Heng Li. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}

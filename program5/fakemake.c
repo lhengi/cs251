c65f4bff23dc10fa95f478e7b8fefcb8e0483077
Heng <lhengi888@gmail.com>
2017-12-04 21:00:36 -0600

    structure created

--- program5/fakemake.c
+++ program5/fakemake.c
@@ -7,18 +7,46 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
+#include "hmap.h"
 
 typedef struct Node
 {
+    int basicFile;
     int timeStamp;
+    int outDegree;
     char* filename;
     struct Node** dependencyFiles;
 }Node;
+
+typedef struct Graph
+{
+    int num_vertex;
+    // hash map of vertexiesc
+    HMAP* hmap;
+}Graph;
+
+Graph* graphCreate()
+{
+    
+    Graph* graph = malloc(sizeof(Graph));
+    graph->num_vertex = 0;
+    graph->hmap = hmap_create(2048,0.75);
+    return graph;
+    
+}
+
+
+
 
 
 int main(int argc, const char * argv[])
 {
     // insert code here...
     printf("Hello, World!\n");
+    
+    int index = 1;
+    char* word;
+    
     return 0;
 }
